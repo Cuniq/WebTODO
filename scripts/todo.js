c157ca6d96276a005ed7eb5a4d4c087b45a2156a
cuniq <argyroudisth@gmail.com>
2018-06-14 20:09:54 +0300

    Added functionality

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,3 +1,6 @@
+const liTodoItemClassname = 'todo-list-item';
+const spanTodoItemClassname = 'todo-list-span';
+
 /*
     Well mr. web browsers are caching input values and it fucking triggers me :)
     Just set it to default god damn it.
@@ -7,14 +10,49 @@ function resetInput() {
     inputText.value = '';
 }
 
-function addNewTodoItemButtonEvent() {
+function addNodesButtonAndInputEvents() {
     const newTodoItemButtons = document.querySelectorAll('.todo-title > button');
     const listItemsWithInput = document.querySelectorAll('.todo-node > ul > li:first-child');
 
     for (let index = 0; index < newTodoItemButtons.length; index++) {
-        const button = newTodoItemButtons[index];
-        button.onclick = function (event) {
-            listItemsWithInput[index].classList.toggle('hidden');
+        const nodeButton = newTodoItemButtons[index];
+        const nodeLiWithInput = listItemsWithInput[index];
+        const input = nodeLiWithInput.children[0];
+
+        nodeButton.onclick = function (event) {
+            nodeLiWithInput.classList.toggle('hidden');
+        };
+
+        nodeLiWithInput.onkeydown = function (event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                if (!input.value) return;
+
+                nodeLiWithInput.parentElement.appendChild(createLiNodeItem(input.value));
+                input.value = '';
+            }
+        };
+    }
+}
+
+function createLiNodeItem(inputValue) {
+    const liNode = document.createElement('li');
+    liNode.classList.add(liTodoItemClassname);
+    const spanNode = document.createElement('span');
+    spanNode.classList.add(spanTodoItemClassname);
+    liNode.appendChild(spanNode);
+    liNode.appendChild(document.createTextNode(inputValue));
+    return liNode;
+}
+
+function setGlobalClickEvents() {
+    document.onclick = function (event) {
+        const element = event.target;
+        if (element.tagName === 'LI' && element.classList.contains(liTodoItemClassname)) {
+            element.classList.toggle('deleted-text');
+        }
+        if (element.tagName === 'SPAN' && element.classList.contains(spanTodoItemClassname)) {
+            element.parentElement.remove();
         }
     }
 }
